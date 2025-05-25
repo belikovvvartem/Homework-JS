91f69132ff8381f0092aada6ecd081d02b7c45c2
Artem <beliko.artem43334@icloud.com>
2025-05-25 21:16:54 +0300

    .

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const taskData = {
-  "2025-05-01": "https://codepen.io/filipz/embed/bNNYyqE?default-tab=html%2Cresult",
-  "2025-05-03": "https://codepen.io/GabbeV/embed/DMRPox?default-tab=html%2Cresult",
-  "2025-05-10": "https://codepen.io/filipz/embed/bNNYyqE?default-tab=html%2Cresult",
-  "2025-05-20": "https://codepen.io/filipz/embed/bNNYyqE?default-tab=html%2Cresult",
-  "2025-05-16": "https://codepen.io/filipz/embed/bNNYyqE?default-tab=html%2Cresult"
+  "2025-05-23": "https://codepen.io/pqendosf-the-decoder/embed/GgJRGER?default-tab=html%2Cresult",
+  "2025-05-24": "https://codepen.io/pqendosf-the-decoder/embed/KwpKeyq?default-tab=js%2Cresult"
 };
 
 const monthSelect = document.getElementById("monthSelect");
@@ -106,66 +103,54 @@ filterBtn.addEventListener("click", () => {
   filterActive = !filterActive;
   filterBtn.textContent = filterActive
     ? "Показати всі дати"
-    : "Показати тільки з завданням";
+    : "Виконані";
   renderCalendar(parseInt(monthSelect.value));
 });
 
 renderCalendar(current.getMonth());
 
-
-
-
-
-
-
-
-
-
-
-
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext('2d');
 
-// for intro motion
 let mouseMoved = false;
 
 const pointer = {
-    x: .5 * window.innerWidth,
-    y: .5 * window.innerHeight,
+  x: .5 * window.innerWidth,
+  y: .5 * window.innerHeight,
 }
 const params = {
-    pointsNumber: 50,
-    widthFactor: .30,
-    mouseThreshold: .6,
-    spring: .4,
-    friction: .5
+  pointsNumber: 50,
+  widthFactor: .30,
+  mouseThreshold: .6,
+  spring: .4,
+  friction: .5
 };
 
 const trail = new Array(params.pointsNumber);
 for (let i = 0; i < params.pointsNumber; i++) {
-    trail[i] = {
-        x: pointer.x,
-        y: pointer.y,
-        dx: 0,
-        dy: 0,
-    }
+  trail[i] = {
+    x: pointer.x,
+    y: pointer.y,
+    dx: 0,
+    dy: 0,
+  }
 }
 
 window.addEventListener("click", e => {
-    updateMousePosition(e.pageX, e.pageY);
+  updateMousePosition(e.pageX, e.pageY);
 });
 window.addEventListener("mousemove", e => {
-    mouseMoved = true;
-    updateMousePosition(e.pageX, e.pageY);
+  mouseMoved = true;
+  updateMousePosition(e.pageX, e.pageY);
 });
 window.addEventListener("touchmove", e => {
-    mouseMoved = true;
-    updateMousePosition(e.targetTouches[0].pageX, e.targetTouches[0].pageY);
+  mouseMoved = true;
+  updateMousePosition(e.targetTouches[0].pageX, e.targetTouches[0].pageY);
 });
 
 function updateMousePosition(eX, eY) {
-    pointer.x = eX;
-    pointer.y = eY;
+  pointer.x = eX;
+  pointer.y = eY;
 }
 
 setupCanvas();
@@ -175,43 +160,42 @@ window.addEventListener("resize", setupCanvas);
 
 function update(t) {
 
-    // for intro motion
-    if (!mouseMoved) {
-        pointer.x = (.5 + .3 * Math.cos(.002 * t) * (Math.sin(.005 * t))) * window.innerWidth;
-        pointer.y = (.5 + .2 * (Math.cos(.005 * t)) + .1 * Math.cos(.01 * t)) * window.innerHeight;
-    }
+  if (!mouseMoved) {
+    pointer.x = (.5 + .3 * Math.cos(.002 * t) * (Math.sin(.005 * t))) * window.innerWidth;
+    pointer.y = (.5 + .2 * (Math.cos(.005 * t)) + .1 * Math.cos(.01 * t)) * window.innerHeight;
+  }
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    trail.forEach((p, pIdx) => {
-        const prev = pIdx === 0 ? pointer : trail[pIdx - 1];
-        const spring = pIdx === 0 ? .4 * params.spring : params.spring;
-        p.dx += (prev.x - p.x) * spring;
-        p.dy += (prev.y - p.y) * spring;
-        p.dx *= params.friction;
-        p.dy *= params.friction;
-        p.x += p.dx;
-        p.y += p.dy;
-    });
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  trail.forEach((p, pIdx) => {
+    const prev = pIdx === 0 ? pointer : trail[pIdx - 1];
+    const spring = pIdx === 0 ? .4 * params.spring : params.spring;
+    p.dx += (prev.x - p.x) * spring;
+    p.dy += (prev.y - p.y) * spring;
+    p.dx *= params.friction;
+    p.dy *= params.friction;
+    p.x += p.dx;
+    p.y += p.dy;
+  });
 
-    ctx.lineCap = "round";
-	 ctx.beginPath();
-    ctx.moveTo(trail[0].x, trail[0].y);
+  ctx.lineCap = "round";
+  ctx.beginPath();
+  ctx.moveTo(trail[0].x, trail[0].y);
 
-    for (let i = 1; i < trail.length - 1; i++) {
-        const xc = .5 * (trail[i].x + trail[i + 1].x);
-        const yc = .5 * (trail[i].y + trail[i + 1].y);
-        ctx.quadraticCurveTo(trail[i].x, trail[i].y, xc, yc);
-        ctx.lineWidth = params.widthFactor * (params.pointsNumber - i);
-        ctx.stroke();
-        ctx.strokeStyle = "lime";
-    }
-    ctx.lineTo(trail[trail.length - 1].x, trail[trail.length - 1].y);
+  for (let i = 1; i < trail.length - 1; i++) {
+    const xc = .5 * (trail[i].x + trail[i + 1].x);
+    const yc = .5 * (trail[i].y + trail[i + 1].y);
+    ctx.quadraticCurveTo(trail[i].x, trail[i].y, xc, yc);
+    ctx.lineWidth = params.widthFactor * (params.pointsNumber - i);
     ctx.stroke();
-    
-    window.requestAnimationFrame(update);
+    ctx.strokeStyle = "lime";
+  }
+  ctx.lineTo(trail[trail.length - 1].x, trail[trail.length - 1].y);
+  ctx.stroke();
+
+  window.requestAnimationFrame(update);
 }
 
 function setupCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 }
