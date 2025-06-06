8872898c0fb1309730c8edf4ad12306ed1394d79
Artem <beliko.artem43334@icloud.com>
2025-06-06 14:34:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -208,8 +208,3 @@ function setupCanvas() {
   canvas.height = window.innerHeight;
 }
 
-function autoUpdateCalendar() {
-  renderCalendar(parseInt(monthSelect.value));
-}
-
-setInterval(autoUpdateCalendar, 10000);
